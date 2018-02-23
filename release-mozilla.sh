861e9fcc6dcb8072198635d1b1ddbebcd7f7b100
David Hoffman <david.hoffman@reancloud.com>
2018-02-23 10:09:12 -0600

    Cleanup

--- release-mozilla.sh
+++ release-mozilla.sh
@@ -2,7 +2,7 @@ cd mozilla-plugin
 
 export version=`cat manifest.json | grep version | grep -v manifest | cut -d':' -f2 | cut -d'"' -f2`
 
-web-ext build
-web-ext sign --api-key=$MOZ_API_KEY --api-secret=$MOZ_API_SECRET
+web-ext build --overwrite-dest
+web-ext sign --api-key=$MOZ_API_KEY --api-secret=$MOZ_API_SECRET 
 
 #https://developer.github.com/v3/repos/releases/#create-a-release
